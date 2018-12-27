36d580c9d14b951731a7c86c3746b9598b31e0b7
KIM ANH <tkimanh3010@gmail.com>
2018-12-27 17:46:00 +0700

    đã đầy đủ api client

--- StartCodingNowWebManager/ApiCommunicationTools/CourseClient.cs
+++ StartCodingNowWebManager/ApiCommunicationTools/CourseClient.cs
@@ -6,13 +6,62 @@ using StartCodingNowWebManager.ApiCommunicationModels.KimAnhAPI;
 
 namespace StartCodingNowWebManager.ApiCommunicationTools
 {
-    public partial class CourseClient
+    public partial class ApiClient
     {
         public List<CourseModel> GetAllCourse()
         {
             var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
-                "Article/GetAllArticles"));
-            return GetAsync<List<ArticleModel>>(requestUrl);
+                "Course/GetAllCourse"));
+            return GetAsync<List<CourseModel>>(requestUrl);
+        }
+
+        public Message<CourseModel> GetCourse(string id)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Course/GetCourse"));
+            return PostAsync<CourseModel, string>(requestUrl, id);
+        }
+
+        public Message<CourseModel> AddCourse(CourseModel model)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Course/AddCourse"));
+            return PostAsync<CourseModel>(requestUrl, model);
+        }
+
+        public Message<CourseModel> UpdateCourse(CourseModel model)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Course/UpdateCourse"));
+            return PostAsync<CourseModel>(requestUrl, model);
+        }
+
+        public Message<CourseModel> DeleteCourse(CourseModel model)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Course/DeleteCourse"));
+            return PostAsync<CourseModel>(requestUrl, model);
+        }
+
+        public Message<CourseModel> SearchCourse(string search)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Course/SearchCourse"));
+            return PostAsync<CourseModel, string>(requestUrl, search);
+        }
+
+        public Message<CourseModel> FilterCoursesByAge(string age)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Course/FilterCoursesByAge"));
+            return PostAsync<CourseModel, string>(requestUrl, age);
+        }
+
+        public Message<CourseModel> FilterCoursesByPrice(string price)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Course/FilterCoursesByPrice"));
+            return PostAsync<CourseModel, string>(requestUrl, price);
         }
     }
 }
---
+++ StartCodingNowWebManager/ApiCommunicationTools/RegisterClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StartCodingNowWebManager.ApiCommunicationModels.KimAnhAPI;
+
+namespace StartCodingNowWebManager.ApiCommunicationTools
+{
+    public partial class ApiClient
+    {
+        public List<RegistrationCourseModel> GetAllRegister()
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Register/GetAllRegister"));
+            return GetAsync<List<RegistrationCourseModel>>(requestUrl);
+        }
+        
+
+        public Message<RegistrationCourseModel> AddRegister(RegistrationCourseModel model)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Register/AddRegister"));
+            return PostAsync<RegistrationCourseModel>(requestUrl, model);
+        }
+
+        public Message<RegistrationCourseModel> UpdateRegister(RegistrationCourseModel model)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "Register/UpdateCourse"));
+            return PostAsync<RegistrationCourseModel>(requestUrl, model);
+        }
+
+        
+    }
+}
